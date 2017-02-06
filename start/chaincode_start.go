64554aa0db77010740d884fbee6cee4e1b3c6503
vltest <vl120116@gmail.com>
2017-02-06 11:24:09 +0800

    Update chaincode_start.go

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -68,7 +68,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 		fmt.Println("hi there " + function)						//error
 		return nil, nil;
 	}
-	fmt.Println("query did not find func: " + function)						//error
+	fmt.Println("query did not find func (test 1): " + function)						//error
 
 	return nil, errors.New("Received unknown function query: " + function)
 }
